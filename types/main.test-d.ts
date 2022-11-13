dc28988547a5f133171a6971ee974af211e3559a
ehmicky <ehmicky@gmail.com>
2022-11-13 15:51:12 +0100

    Improve types

--- types/main.test-d.ts
+++ types/main.test-d.ts
@@ -1,12 +1,25 @@
-import modernErrors from 'modern-errors'
+import ModernError from 'modern-errors'
 import modernErrorsBugs from 'modern-errors-bugs'
 import { expectType, expectError } from 'tsd'
 
-const BaseError = modernErrors([modernErrorsBugs])
+const BaseError = ModernError.subclass('BaseError', {
+  plugins: [modernErrorsBugs],
+})
 const error = new BaseError('', { cause: '' })
 
-modernErrors([modernErrorsBugs], { bugs: 'https://example.com' })
-modernErrors([modernErrorsBugs], { bugs: new URL('https://example.com') })
-expectError(modernErrors([modernErrorsBugs], { bugs: true }))
+ModernError.subclass('TestError', {
+  plugins: [modernErrorsBugs],
+  bugs: 'https://example.com',
+})
+ModernError.subclass('TestError', {
+  plugins: [modernErrorsBugs],
+  bugs: new URL('https://example.com'),
+})
+expectError(
+  ModernError.subclass('TestError', {
+    plugins: [modernErrorsBugs],
+    bugs: true,
+  }),
+)
 
 expectType<string>(error.message)
